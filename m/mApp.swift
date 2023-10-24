f68a31c7103b4f980e934b85598ee8235cb5e6c0
Alsaad <mrrgx.22@gmail.com>
2023-10-24 10:42:55 +0300

    Initial Commit

---
+++ m/mApp.swift
@@ -0,0 +1,17 @@
+//
+//  mApp.swift
+//  m
+//
+//  Created by Mohammed on 24/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct mApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
